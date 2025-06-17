3d4bf01639ffeda015059c5f3796824fd7347889
rollm8789 <rollm8789@student.faytechcc.edu>
2025-06-16 20:24:08 -0400

    Add files via upload

---
+++ P1LAB1_RollMatt.py
@@ -0,0 +1,12 @@
+
+# Matthew Roll
+# Todays Date: June 16, 2025
+# P1LAB1
+# This program prompts the user to enter their first and last name separately,then displays a welcome message using the entered names.
+
+# Prompt the user for their first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display a welcome message
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110-1902,")
